91e6b98bdcf594173fb636c18893143812e3a799
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-12-15 15:59:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,35 +8,36 @@ client.on('ready', () => {
 });
 
 
-const devs1 = ["480407581085532180"];
+const devs1 = ["288368249194938368"];
 
 client.on('message', message => {
     let argresult = message.content.split(` `).slice(1).join(' ');
     if (message.content.startsWith(prefix + 'setStreaming')) {
-      if (!devs1.includes(message.author.id)) return message.channel.send("<@480407581085532180> only this guy can do restart the bot so don't try again :wink:.");
+      if (!devs1.includes(message.author.id)) return message.channel.send("<@288368249194938368> only this guy can do restart the bot so don't try again :wink:.");
       message.delete();
+       
       client.user.setGame(argresult, 'https://twitch.tv/DynastyShop');
 
     } else if(message.content.startsWith(prefix + 'setWatching')) {
         client.user.setActivity(argresult,{type: 'WATCHING'});
-
+message.reply(`Done changed to ${argresult}`)
       } else if(message.content.startsWith(prefix + 'setListening')) {
         client.user.setActivity(argresult,{type: 'LISTENING'});
-
+message.reply(`Done changed to ${argresult}`)
       } else if(message.content.startsWith(prefix + 'setPlaying')) {
         client.user.setActivity(argresult,{type: 'PLAYING'});
-
+message.reply(`Done changed to ${argresult}`)
       } else if(message.content.startsWith(prefix + 'setName')) {
         client.user.setUsername(argresult);
-
+message.reply(`Done changed to ${argresult}`)
       } else if(message.content.startsWith(prefix + 'setAvatar')) {
         client.user.setAvatar(argresult);
-
+message.reply(`Done changed to ${argresult}`)
 
       } else if(message.content.startsWith(prefix + 'setStatus')) {
         if(!argresult) return message.channel.send('`online`, `DND(Do not Distrub),` `idle`, `invisible(Offline)` :notes: أختر أحد الحالات');
         client.user.setStatus(argresult);
-
+message.reply(`Done changed to ${argresult}`)
 
     }
 
